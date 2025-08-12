2e84f6d3046d693de2c854b0b07fd4fbff4693e5
Nick Liggett <nick.liggett@gmail.com>
2025-08-11 23:01:13 -0600

    Build basic user grid

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
-
+displayedColumns: string[] = ['id', 'firstName', 'lastName', 'email', 'role'];
+dataSource = [
+  {
+    id: '123',
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'email@example.com',
+    role: 'ADMIN'
+  }
+];
 }
